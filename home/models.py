d436385e4d4027a53cd8c0b058da0947b23c956c
algorithm-sys <marlvex369@gmail.com>
2024-07-26 07:48:29 +0300

    00010

--- home/models.py
+++ home/models.py
@@ -9,21 +9,44 @@ from .blocks import Carousel, PostStreamBlock, AccomodationBody, ImageStream
 
 
 class HomePage(Page):
-    carousel = StreamField(Carousel(), use_json_field=True, blank=True, null=True)
+    carousel = StreamField(
+        Carousel(),
+        use_json_field=True,
+        blank=True,
+        null=True,
+    )
 
-    introduction_title = models.CharField(max_length=255, null=True, blank=True)
-    introduction_subheading = models.TextField(null=True, blank=True)
-    introduction_cta_text = models.CharField(max_length=255, null=True, blank=True)
+    introduction_title = models.CharField(
+        "Main Heading",
+        max_length=255,
+        null=True,
+        blank=True,
+        help_text="Enter the primary headline for the homepage introduction",
+    )
+    introduction_subheading = models.TextField(
+        "Subheading",
+        null=True,
+        blank=True,
+        help_text="Provide a brief description or tagline to appear below the main heading.",
+    )
+    introduction_cta_text = models.CharField(
+        "Call to Action Button Text",
+        help_text="Enter the text you want to display on the call-to-action button (e.g., 'Learn More', 'Book Now",
+        max_length=255,
+        null=True,
+        blank=True,
+    )
     introduction_cta_link = models.ForeignKey(
         "wagtailcore.Page",
         related_name="+",
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
+        help_text="Call to Action Button Link",
     )
 
     featured_section_one = StreamField(
-        PostStreamBlock(), use_json_field=True, null=True, blank=True
+        PostStreamBlock(), use_json_field=True, null=True, blank=True, help_text=""
     )
     featured_section_two = StreamField(
         PostStreamBlock(), use_json_field=True, null=True, blank=True
@@ -67,7 +90,8 @@ class HomePage(Page):
             [
                 FieldPanel("carousel"),
             ],
-            heading="Carousel Section",
+            heading="Homepage Carousel",
+            help_text="Upload images to display in a rotating carousel at the top of the homepage. Recommended image dimensions: 1920px width x 1080px height.",
         ),
         MultiFieldPanel(
             [
@@ -80,41 +104,87 @@ class HomePage(Page):
         ),
         MultiFieldPanel(
             [
-                FieldPanel("featured_section_one"),
-                FieldPanel("featured_section_two"),
+                FieldPanel(
+                    "featured_section_one",
+                    heading="Featured Retreat Category 1",
+                    help_text="Choose retreats to highlight in this section. Example categories: 'Family Getaways', 'Romantic Escapes', 'Wellness Retreats",
+                ),
+                FieldPanel(
+                    "featured_section_two",
+                    heading="Featured Retreat Category 2",
+                    help_text="Choose retreats to highlight in this section. Example categories: 'Adventure Packages', 'Group Specials', 'Last-Minute Deals",
+                ),
             ],
-            heading="Featured Pages",
+            heading="Featured Categories",
         ),
-        FieldPanel("section_three"),
+        FieldPanel(
+            "section_three",
+            heading="Special Offer Text",
+            help_text="Enter text to highlight a special offer or promotion (e.g., 'View Our Latest Packages').",
+        ),
         MultiFieldPanel(
             [
-                FieldPanel("accomodation_section_title"),
+                FieldPanel("accomodation_section_title", heading="Highlights Title"),
                 FieldPanel("accomodation_section_subheading"),
-                FieldPanel("accommodation_section_image"),
-                InlinePanel("acc_features", label="Feature"),
-                FieldPanel("accomodation_section_paragraph"),
-                FieldPanel("accomodation_section_cta_link"),
+                FieldPanel(
+                    "accommodation_section_image",
+                    heading="Featured Image",
+                    help_text="",
+                ),
+                InlinePanel(
+                    "acc_features",
+                    label="Highlights",
+                    help_text="Use bullet points, short phrases, or icons to showcase key features, benefits, or information.",
+                ),
+                FieldPanel(
+                    "accomodation_section_paragraph",
+                ),
+                FieldPanel(
+                    "accomodation_section_cta_link",
+                    heading="Call to Action Button Link",
+                ),
             ],
-            heading="Accomodation Section",
+            heading="Featured Image & Highlights ",
+            help_text="This section uses a split-screen layout, ideal for visually presenting key features or selling points. One side prominently features an image, while the other uses concise text, bullet points, or icons.",
         ),
         MultiFieldPanel(
             [
-                FieldPanel("location_section_title"),
-                FieldPanel("location_section_subheading"),
-                InlinePanel("locations", max_num=2),
+                FieldPanel("location_section_title", heading="Showcase Title"),
+                FieldPanel(
+                    "location_section_subheading", heading="Showcase Subheading"
+                ),
+                InlinePanel(
+                    "locations",
+                    max_num=2,
+                    heading="Showcase Items",
+                    help_text="Add content for each item you want to feature in the two-column grid",
+                ),
             ],
-            heading="Location Section",
+            heading="Two-Column Showcase",
+            help_text="This section employs a two-column grid to present content in a visually balanced way. Perfect for showcasing multiple locations, activities, services, or categories with images and descriptions.",
         ),
         MultiFieldPanel(
             [FieldPanel("services_section_title"), InlinePanel("services", max_num=3)],
-            heading="Services Section",
+            heading="Three-Up Feature Grid",
+            help_text="This section utilizes a three-column grid to highlight additional options, services, or categories in a concise and scannable format. Each item typically includes an image and a short description.",
         ),
         # MultiFieldPanel(
         #     [InlinePanel("icon_links", max_num=6)],
         #     heading="Icon Links Section",
         # ),
         MultiFieldPanel(
-            [FieldPanel("styled_link_text"), FieldPanel("styled_link_text_link")],
+            [
+                FieldPanel(
+                    "styled_link_text",
+                    heading="Special Offer Text",
+                    help_text="Enter text to highlight a special offer or promotion (e.g., 'View Our Latest Packages').",
+                ),
+                FieldPanel(
+                    "styled_link_text_link",
+                    heading="Special Offer Link",
+                    help_text="Select the page where users can find more details about the offer.",
+                ),
+            ],
             heading="Styled Link Section",
         ),
     ]
@@ -174,8 +244,21 @@ class Location(models.Model):
         related_name="+",
     )
 
-    name = models.CharField(max_length=255)
-    text = models.TextField(null=True, blank=True)
+    name = models.CharField(
+        "Item Title",
+        max_length=255,
+        help_text="Provide a short, descriptive title for this item",
+    )
+    text = models.TextField(
+        "Item Description",
+        null=True,
+        blank=True,
+        help_text="Add a brief description of the item",
+    )
+
+    class Meta:
+        verbose_name = "Item"
+        verbose_name_plural = "Items"
 
 
 class AccomodationFeature(models.Model):
@@ -183,3 +266,7 @@ class AccomodationFeature(models.Model):
     page = ParentalKey(
         "HomePage", related_name="acc_features", on_delete=models.CASCADE
     )
+
+    class Meta:
+        verbose_name = "Feauture"
+        verbose_name_plural = "Features"
